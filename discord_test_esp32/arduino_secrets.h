46f77b050fdeb9e61c14070e458133a3411ddf34
Ayoub-ben <49066193+Ayoub-ben@users.noreply.github.com>
2019-05-02 19:41:48 +0400

    Add files via upload

---
+++ discord_test_esp32/arduino_secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "iPhone de Ayoub"
+#define SECRET_PASS "ayoub063"
+#define SECRET_WEBHOOK "https://discordapp.com/api/webhooks/570236438096904202/1TGtg9Lm7y4Ocsa_BdcQnmWmEGub8Hv23LJQVB-CTo8Z__4PcOb4TjJXQ3sf1tARpZCs"
+#define SECRET_TTS "false"
